4b4a05963b76d8aacc318d32bcdb31bf3b6d7dfd
Twin Twist <stukupl@gmail.com>
2017-06-27 22:30:36 +0200

    Introduce a Buggy Class just 4 fun

---
+++ src/main/java/org/shipkit/bootstrap/BuggyClass.java
@@ -0,0 +1,24 @@
+package org.shipkit.bootstrap;
+
+import java.util.Random;
+
+public class BuggyClass {
+
+    private int number;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BuggyClass)) return false;
+
+        BuggyClass that = (BuggyClass) o;
+
+        return number == that.number;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return new Random().nextInt();
+    }
+}
