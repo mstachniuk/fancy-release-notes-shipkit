cba3b24770675b8b80e084efd7661b5b1c0605b1
Hephaestus Damocles <mstachniuk+HephaestusDamoclesGithub@gmail.com>
2017-06-27 22:06:48 +0200

    Use Java 8 in printing out

--- src/main/java/org/shipkit/bootstrap/HelloWorld.java
+++ src/main/java/org/shipkit/bootstrap/HelloWorld.java
@@ -1,8 +1,11 @@
 package org.shipkit.bootstrap;
 
+import java.util.Optional;
+
 public interface HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Ship it!");
+        Optional.of("Shipkit is the best").ifPresent(System.out::println);
     }
 }
